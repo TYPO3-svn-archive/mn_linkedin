8f3fb4c00cca2ccef43054a46bc2aaced7decde6
mattias.nilsson <mattias.nilsson@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-10-13 18:57:14 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mn_linkedin/trunk@52981 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "mn_linkedin".
 #
-# Auto generated 23-09-2011 09:17
+# Auto generated 13-10-2011 16:59
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -20,14 +20,14 @@ $EM_CONF[$_EXTKEY] = array(
 	'shy' => '',
 	'priority' => '',
 	'module' => '',
-	'state' => 'alpha',
+	'state' => 'beta',
 	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '0.1.0',
+	'version' => '0.2.0',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:24:{s:21:"ExtensionBuilder.json";s:4:"06e1";s:12:"ext_icon.gif";s:4:"e922";s:17:"ext_localconf.php";s:4:"0e6d";s:14:"ext_tables.php";s:4:"ac32";s:14:"ext_tables.sql";s:4:"5d3c";s:52:"Classes/Controller/LinkedinApplicationController.php";s:4:"a915";s:44:"Classes/Domain/Model/LinkedinApplication.php";s:4:"3c93";s:59:"Classes/Domain/Repository/LinkedinApplicationRepository.php";s:4:"536a";s:44:"Configuration/ExtensionBuilder/settings.yaml";s:4:"dc9b";s:41:"Configuration/TCA/LinkedinApplication.php";s:4:"a81c";s:38:"Configuration/TypoScript/constants.txt";s:4:"e2bf";s:34:"Configuration/TypoScript/setup.txt";s:4:"6831";s:40:"Resources/Private/Language/locallang.xml";s:4:"6819";s:91:"Resources/Private/Language/locallang_csh_tx_mnlinkedin_domain_model_linkedinapplication.xml";s:4:"4b48";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"1184";s:38:"Resources/Private/Layouts/Default.html";s:4:"debc";s:57:"Resources/Private/Templates/LinkedinApplication/List.html";s:4:"c837";s:57:"Resources/Private/Templates/LinkedinApplication/Show.html";s:4:"4b48";s:36:"Resources/Public/Css/mn_linkedin.css";s:4:"28a5";s:35:"Resources/Public/Icons/relation.gif";s:4:"e615";s:73:"Resources/Public/Icons/tx_mnlinkedin_domain_model_linkedinapplication.gif";s:4:"905a";s:59:"Tests/Unit/Controller/LinkedinApplicationControllerTest.php";s:4:"e30d";s:51:"Tests/Unit/Domain/Model/LinkedinApplicationTest.php";s:4:"1909";s:14:"doc/manual.sxw";s:4:"fb17";}',
+	'_md5_values_when_last_written' => 'a:23:{s:21:"ExtensionBuilder.json";s:4:"06e1";s:12:"ext_icon.gif";s:4:"e922";s:17:"ext_localconf.php";s:4:"0e6d";s:14:"ext_tables.php";s:4:"ac32";s:14:"ext_tables.sql";s:4:"5d3c";s:52:"Classes/Controller/LinkedinApplicationController.php";s:4:"a915";s:44:"Classes/Domain/Model/LinkedinApplication.php";s:4:"3c93";s:59:"Classes/Domain/Repository/LinkedinApplicationRepository.php";s:4:"536a";s:44:"Configuration/ExtensionBuilder/settings.yaml";s:4:"dc9b";s:41:"Configuration/TCA/LinkedinApplication.php";s:4:"a81c";s:38:"Configuration/TypoScript/constants.txt";s:4:"e2bf";s:34:"Configuration/TypoScript/setup.txt";s:4:"6831";s:40:"Resources/Private/Language/locallang.xml";s:4:"6819";s:91:"Resources/Private/Language/locallang_csh_tx_mnlinkedin_domain_model_linkedinapplication.xml";s:4:"4b48";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"1184";s:38:"Resources/Private/Layouts/Default.html";s:4:"debc";s:57:"Resources/Private/Templates/LinkedinApplication/List.html";s:4:"c837";s:57:"Resources/Private/Templates/LinkedinApplication/Show.html";s:4:"4b48";s:35:"Resources/Public/Icons/relation.gif";s:4:"e615";s:73:"Resources/Public/Icons/tx_mnlinkedin_domain_model_linkedinapplication.gif";s:4:"905a";s:59:"Tests/Unit/Controller/LinkedinApplicationControllerTest.php";s:4:"e30d";s:51:"Tests/Unit/Domain/Model/LinkedinApplicationTest.php";s:4:"1909";s:14:"doc/manual.sxw";s:4:"fb17";}',
 );
 
 ?>
